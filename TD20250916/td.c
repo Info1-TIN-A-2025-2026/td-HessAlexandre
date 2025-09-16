bbf69aa887f46434f3b5fcb968b167c17c778cab
HessAlexandre <alexandre.hess@heig-vd.ch>
2025-09-16 12:32:35 +0000

    cours du jour, 16.09.25

---
+++ TD20250916/td.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+
+	unsigned int num_students = 23;
+	signed int account_value = -42;		// signed est optionnel; on pourrait mettre int m = -42
+	float temperature = 18.1;			// Float, température,				32 bit
+	double pi = 3.14159265358979323846; // Double, valeur de pi,	 	64 bit
+}
