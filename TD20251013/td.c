4badd8f9bda04f629cdfb603e9a0ad856449a5db
HessAlexandre <alexandre.hess@heig-vd.ch>
2025-10-13 11:46:51 +0000

    V1.0

--- TD20251013/td.c
+++ TD20251013/td.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, const char *argv[])
 {
-	
 
-	
+    int d1 = 0; //Set D1 to 0
+    int d2 = 0; //Set D2 to 0
+
+    long Call_Price = 0; // Set Long as price might be with multiple decimals
 }
