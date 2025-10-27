c9ea5c5960a233f0b2a2a8ac4420377b57d1590d
HessAlexandre <alexandre.hess@heig-vd.ch>
2025-10-27 15:14:52 +0000

    Added the prime number calculator

--- TD20251027/td.c
+++ TD20251027/td.c
@@ -21,11 +21,38 @@ int main(int argc, const char *argv[])
 
 // }
 
-{
-	int a = 10;
-	printf("%d\n", a++); // Post incrementation
+//		Série 2:
+// {
+// 	int a = 10;
+// 	printf("%d\n", a++); // Post incrementation
 
-	a = 10;
-	printf("%d\n", ++a);
-	return 0; // Pre incrementation
+// 	a = 10;
+// 	printf("%d\n", ++a);
+// 	return 0; // Pre incrementation
+// }
+{
+	int n = 0;
+	int cnt = 0;
+	int i = 0;
+
+	for (n = 2; n <= 100; n++)
+	{
+
+		cnt = 0;
+
+		// Compter tous les diviseurs de n
+		for (i = 1; i <= n; i++)
+		{
+
+			// Vérifier si n est divisible par i
+			if (n % i == 0)
+				cnt++;
+		}
+
+		// Si n n'a que 2 diviseurs (1 et lui-même), il est premier.
+		if (cnt == 2)
+			printf("%d is prime\n", n);
+	}
+
+	return 0;
 }
