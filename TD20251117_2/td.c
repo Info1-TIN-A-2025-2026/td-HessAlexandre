9903777e20fff0c87d2e43da5041057e9448fe08
HessAlexandre <alexandre.hess@heig-vd.ch>
2025-11-17 14:17:56 +0000

    added 20251117_2

---
+++ TD20251117_2/td.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+double delta(const double a, const double b, const double c);
+
+int main(int argc, const char *argv[])
+{
+	double a = 0;
+	double b = 0;
+	double c = 0;
+	double result = 0;
+	printf("Entrez 3 valeures entières\n");
+	scanf("%lf%lf%lf", &a, &b, &c); // Ne pas rajouter de virgules dans la zone de saisie,
+	// sinon il faut aussi ajouter des virgules après dans la zone de texte !
+	result = delta(a, b, c);
+	printf("%.0lf", result);
+	return 0;
+}
+
+double delta(double a, double b, double c)
+{
+	double r = 0.;
+	r = b * b - 4. * a * c;
+	return r;
+}
